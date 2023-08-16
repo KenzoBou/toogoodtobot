1731fa45077a8a2286bdd1eaca455996e33ff30c
KenzoBou <82337503+KenzoBou@users.noreply.github.com>
2023-08-16 22:57:51 +0200

    Update toogoodtobot.py

--- toogoodtobot.py
+++ toogoodtobot.py
@@ -13,7 +13,7 @@ credentials = client.get_credentials()
 client = TgtgClient(access_token='e30.eyJzdWIiOiIxMTI0MDc4NjciLCJleHAiOjE2OTE5NDA1NTMsInQiOiJMSnd2SWpiVVRKZTNDa2hSRDlDMm5BOjA6MSJ9.LRYOSo944p79_Ei03AaeudBGIrJRlq8rmAc5o1Vg8do', 
                     refresh_token='e30.eyJzdWIiOiIxMTI0MDc4NjciLCJleHAiOjE3MjMzOTAxNTMsInQiOiJlUGFUcmUxNFRHS2tsOXpqZXlsdXZBOjA6MCJ9.BS4lm0elLI-vtbf8dQdUSwOZ-SQ0_tt0iWDNmDuJ6wE',
                     user_id="112407867", 
-                    cookie='datadome=4s4F5UKgeELddOGszCx~1z3C~EKZiXBKgUop_4-3tuQufoGNoAr5B0WJEQQ143BDGJ39BKDSgW_ug3UVl6aQ4pYSc4WLbZlS6w3Z6WI~StFWXdp1~F9pyEDwVCZzhd7b; Max-Age=5184000; Domain=.apptoogoodtogo.com; Path=/; Secure; SameSite=Lax'')
+                    cookie='datadome=4s4F5UKgeELddOGszCx~1z3C~EKZiXBKgUop_4-3tuQufoGNoAr5B0WJEQQ143BDGJ39BKDSgW_ug3UVl6aQ4pYSc4WLbZlS6w3Z6WI~StFWXdp1~F9pyEDwVCZzhd7b; Max-Age=5184000; Domain=.apptoogoodtogo.com; Path=/; Secure; SameSite=Lax')
 
 items = client.get_items()
 
