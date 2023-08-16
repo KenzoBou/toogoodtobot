340bccd967bbb9ac1c862260484be39dbe5a3e4f
KenzoBou <82337503+KenzoBou@users.noreply.github.com>
2023-08-16 22:57:17 +0200

    Update toogoodtobot.py

--- toogoodtobot.py
+++ toogoodtobot.py
@@ -7,10 +7,13 @@
 from tgtg import TgtgClient
 import requests
 
+client = TgtgClient(email="guillaume.kravitz@gmail.com")
+credentials = client.get_credentials()
+
 client = TgtgClient(access_token='e30.eyJzdWIiOiIxMTI0MDc4NjciLCJleHAiOjE2OTE5NDA1NTMsInQiOiJMSnd2SWpiVVRKZTNDa2hSRDlDMm5BOjA6MSJ9.LRYOSo944p79_Ei03AaeudBGIrJRlq8rmAc5o1Vg8do', 
                     refresh_token='e30.eyJzdWIiOiIxMTI0MDc4NjciLCJleHAiOjE3MjMzOTAxNTMsInQiOiJlUGFUcmUxNFRHS2tsOXpqZXlsdXZBOjA6MCJ9.BS4lm0elLI-vtbf8dQdUSwOZ-SQ0_tt0iWDNmDuJ6wE',
                     user_id="112407867", 
-                    cookie='datadome=2lqnRUe6C~ctcLOz_iTU-92MWRcLfLb2CLjzurbAFzyWLthbr~Bhw43Qyf4TguB1TD~FzQ199zfrAz2pFoIx~4yikyPqREsqYDZOOoVgDTPeG-FSOqGOv-B4Oc0WBEuO')
+                    cookie='datadome=4s4F5UKgeELddOGszCx~1z3C~EKZiXBKgUop_4-3tuQufoGNoAr5B0WJEQQ143BDGJ39BKDSgW_ug3UVl6aQ4pYSc4WLbZlS6w3Z6WI~StFWXdp1~F9pyEDwVCZzhd7b; Max-Age=5184000; Domain=.apptoogoodtogo.com; Path=/; Secure; SameSite=Lax'')
 
 items = client.get_items()
 
